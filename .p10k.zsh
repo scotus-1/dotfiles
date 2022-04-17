4955c5cf30e61c826ba82e0373243158ea400f72
Titus Nguyen <titusstar007@gmail.com>
2022-04-17 13:19:05 -0700

    Changed some terminal colors

--- .p10k.zsh
+++ .p10k.zsh
@@ -554,7 +554,7 @@
 
   #######################[ background_jobs: presence of background jobs ]#######################
   # Background jobs color.
-  typeset -g POWERLEVEL9K_BACKGROUND_JOBS_FOREGROUND=6
+  typeset -g POWERLEVEL9K_BACKGROUND_JOBS_FOREGROUND=5
   typeset -g POWERLEVEL9K_BACKGROUND_JOBS_BACKGROUND=0
   # Don't show the number of background jobs.
   typeset -g POWERLEVEL9K_BACKGROUND_JOBS_VERBOSE=false
